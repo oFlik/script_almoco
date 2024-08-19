d5d2072aae308dbcb4268fded5e8e14aab2dcca9
Thiago Pereira <iamthiago2@gmail.com>
2024-08-19 11:44:38 -0300

    Fixed .env loading in executable file

--- src/config/basic_info.py
+++ src/config/basic_info.py
@@ -1,7 +1,13 @@
 from dotenv import load_dotenv
+import sys
 import os
 
-load_dotenv(override=True)
+#load_dotenv(override=True)
+
+extDataDir = os.getcwd()
+if getattr(sys, 'frozen', False):
+    extDataDir = sys._MEIPASS
+load_dotenv(override=True, dotenv_path=os.path.join(extDataDir, '.env'))
 
 NAME = os.getenv("NAME")
 FORM_LINK = os.getenv("FORM_LINK")
