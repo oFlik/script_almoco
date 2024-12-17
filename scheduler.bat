518609097a2a98a117ed2404d94a6ea17ba54787
Thiago Pereira <iamthiago2@gmail.com>
2024-12-17 10:54:01 -0300

    Added skip to empty division

---
+++ scheduler.bat
@@ -0,0 +1,3 @@
+C:\Users\thiago.pereira\Projetos\Python\script_almoco\venv\Scripts\activate.bat 
+C:\> timeout 5 >nul
+python C:\Users\thiago.pereira\Projetos\Python\script_almoco\app.py
--- src/main.py
+++ src/main.py
@@ -5,12 +5,27 @@ from .filler import fill_form
 
 
 def start_filler():
+    ask_to_skip = True
     for division in divisions:
+
         try:
+            d_data = get_div_data(division)
+
+            if d_data[1] == 0:
+                print(f'''Divisão {
+                      division} não possui funcionários para almoçar''')
+                continue
+
+            if ask_to_skip == True:
+                skip = input(f'Divisão: {division}. Pular? S ou N: ')
+
+                if skip == 'N' or skip == 'n':
+                    ask_to_skip = False
+                elif skip == 'S' or skip == 's':
+                    continue
+
             browser = config_webdriver()
             browser.get(FORM_LINK)
-
-            d_data = get_div_data(division)
 
             fill_form(browser, division, d_data)
         except Exception as int_err:
